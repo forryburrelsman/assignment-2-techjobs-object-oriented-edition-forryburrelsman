3098aa005660a1330e38a7d7a3c4e2c029a99456
Forrest Burrelsman <forrestburrelsman@gmail.com>
2021-10-21 20:06:48 -0500

    added testJobsForEquality

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,11 +1,12 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.*;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -22,9 +23,27 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job.getName() instanceof String);
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(job.getName(), "Product tester");
+        assertEquals(job.getEmployer().getValue(), "ACME");
+        assertEquals(job.getLocation().getValue(), "Desert");
+        assertEquals(job.getPositionType().getValue(), "Quality control");
+        assertEquals(job.getCoreCompetency().getValue(), "Persistence");
+    }
 
+    @Test
+    public void testJobsForEquality(){
+        Job job3 = new Job("Stay at Home Dad", new Employer("Cats"), new Location("Home"),
+                new PositionType("Remote"), new CoreCompetency("Extra Skilled"));
+        Job job4 = new Job("Stay at Home Dad", new Employer("Cats"), new Location("Home"),
+                new PositionType("Remote"), new CoreCompetency("Extra Skilled"));
+        assertNotEquals(job3, job4);
     }
 
 }
